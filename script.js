32b2c7e1b40f97cf87295865a42d38a3079be1a1
LmnopEmc2 <110025657+LmnopEmc2@users.noreply.github.com>
2025-03-02 21:11:10 +0330

    simple mini app from AP

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-// script.js
+// // script.js
+// Telegram.WebApp.ready();
+
+// Telegram.WebApp.onEvent("mainButtonClicked", function () {
+//   Telegram.WebApp.sendData("hello");
+// });
+
+// let mainButton = Telegram.WebApp.MainButton.setParams({
+//   text: "Send data to Bot",
+// }).onClick(function () {
+//   // You can do something on button click,
+//   // for example, close the Web App:
+//   Telegram.WebApp.close();
+// });
+
+// Initialize the Mini App
 Telegram.WebApp.ready();
 
-Telegram.WebApp.onEvent("mainButtonClicked", function () {
-  Telegram.WebApp.sendData("hello");
-});
+// Get the modal
+var modal = document.getElementById("myModal");
 
-let mainButton = Telegram.WebApp.MainButton.setParams({
-  text: "Send data to Bot",
-}).onClick(function () {
-  // You can do something on button click,
-  // for example, close the Web App:
-  Telegram.WebApp.close();
-});
+// Get the button that opens the modal
+var btn = document.getElementById("showPopup");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
